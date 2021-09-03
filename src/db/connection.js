063782f1a362cafc100862a3ae8e1f59fc817522
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-09-03 22:47:08 +0200

    join,deploy to heroku

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,6 +2,14 @@ import pg from 'pg'
 
 const {Pool} = pg
 
-const db = new Pool()
+const db = new Pool({
+	ssl: {
+		rejectUnauthorized: false,
+	},
+	connectionString:
+		process.env.NODE_ENV !== "development"
+			? process.env.DATABASE_URL
+			: process.env.DATABASE_URL_DEV,
+})
 
 export default db
--- src/services/reviews/handlers.js
+++ src/services/reviews/handlers.js
@@ -4,7 +4,21 @@ import db from "../../db/connection.js";
 
 export const list = async (req,res,next)=>{
     try {
-        const reviews = await db.query(`SELECT * FROM reviews`)
+        const reviews = await db.query(`SELECT 
+        review.review_id,
+        review.product_id ,
+        review.comment,
+        review.rate,
+        review.created_at,
+        product.product_id,
+        product.name,
+        product.description,
+        product.brand,
+        product.image_url,
+        product.price
+        FROM reviews AS review
+        INNER JOIN products AS product ON review.review_id=product.product_id 
+        ORDER BY review.created_at DESC;`)
         res.send(reviews.rows)
     } catch (error) {
         res.status(500).send(error)
@@ -14,7 +28,22 @@ export const list = async (req,res,next)=>{
 export const single = async(req,res,next)=>{
     try {
         const {review_id} = req.params
-        const reviews = await db.query(`SELECT * FROM reviews WHERE review_id=${review_id}`)
+        const reviews = await db.query(`SELECT 
+        review.review_id,
+        review.product_id ,
+        review.comment,
+        review.rate,
+        review.created_at,
+        product.product_id,
+        product.name,
+        product.description,
+        product.brand,
+        product.image_url,
+        product.price
+        FROM reviews AS review
+        INNER JOIN products AS product ON review.review_id=product.product_id 
+        WHERE review.review_id = '${review_id}'
+        ORDER BY review.created_at DESC;`)
         const [found, ...rest] = reviews.rows
         res.status(found?200:400).send(found)
     } catch (error) {
